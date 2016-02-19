70b563b57547f51d00cd2a2024ba756057436717
fliem <franz.liem>
2016-02-19 17:46:04 +0100

    intitial

--- run_72_collect_results_df.py
+++ run_72_collect_results_df.py
@@ -1,99 +1,91 @@
 __author__ = 'franzliem'
 
-import os
+import os, glob
 import pandas as pd
+from variables import ds_root_path
+import seaborn as sns
+import pylab as plt
+import numpy as np
 
-# # LeiCA modules
-from learning.learning_prepare_data_wf import learning_prepare_data_wf
+ds_dir = os.path.join(ds_root_path, 'learning_out_test/pdfs')
 
-from variables import wd_root_path, ds_root_path, data_lookup_dict, template_lookup_dict
-from variables import behav_file, qc_file
+os.chdir(ds_dir)
+df = None
+for reg in ['01_split_results_error', '03_split_regFDresults_error']:
+    df_list = glob.glob(reg + '/*.txt')
+    for df_file in df_list:
+        df_in = pd.read_csv(df_file, index_col=0)
+        if df is None:
+            df = df_in
+        else:
+            df = df.append(df_in)
 
-working_dir = os.path.join(wd_root_path, 'wd_learning_test')
-ds_dir = os.path.join(ds_root_path, 'learning_out_test')
+scatter_df = {}
+ana_list = np.unique(df.index.values)
+for reg_path, reg in {'01_split_predicted': False, '03_split_regFDpredicted': True}.items():
+    scatter_df[reg] = {}
+    for a in ana_list:
+        scatter_df[reg][a] = pd.read_pickle(os.path.join(ds_dir, reg_path, a + '_df_predicted.pkl'))
 
-use_n_procs = 25
-plugin_name = 'MultiProc'
+all_ana = set(df.query('MAE_test < 20').index)
+df_all= df.loc[all_ana]
+df_all.ix[:,'ana']  = df_all.index.values
+multi_index = pd.MultiIndex.from_tuples(zip(df_all['ana'], df_all['FD_res']), names=['ana_id', 'FD_red'])
+df_all.set_index(multi_index, inplace=True)
+df_all.sort_index(inplace=True)
+df_all.to_csv('00_df_all.csv')
 
+if len(all_ana) < 20:
+    plt.figure()
+    ax = sns.barplot(x="MAE_test", y="ana", data=df_all, hue='FD_res')
+    plt.tight_layout()
+    plt.savefig('00_all_bars_MAE.pdf')
 
-
-# # # # # # # # # #
-# subject selection criteria
-# # # # # # # # # #
-subjects_selection_crit_dict = {}
-# subjects_selection_crit_dict['adult_healthy_F'] = ["df[df.sex == \'F\']", 'df[df.no_axis_1]', 'df[df.age >= 18]']
-# subjects_selection_crit_dict['adult_F'] = ["df[df.sex == \'F\']", 'df[df.age >= 18]']
-subjects_selection_crit_dict['bothSexes'] = ["df[df.n_TRs>294]"]
-subjects_selection_crit_dict['bothSexes_FD01_035'] = ["df[df.n_TRs>294]", "df[df.mean_FD_P<.35]", "df[df.mean_FD_P>.1]"]
-subjects_selection_crit_dict['bothSexes_FD01_035_age50plus'] = ["df[df.n_TRs>294]", "df[df.mean_FD_P<.35]",
-                                                                "df[df.mean_FD_P>.1]", "df[df.age>50]"]
-
-subjects_selection_crit_dict['F'] = ["df[df.sex == \'F\']", "df[df.n_TRs>294]"]
-subjects_selection_crit_dict['F_fd04'] = ["df[df.sex == \'F\']", "df[df.n_TRs>294]", "df[df.mean_FD_P<.4]"]
-subjects_selection_crit_dict['F_fd01'] = ["df[df.sex == \'F\']", "df[df.n_TRs>294]", "df[df.mean_FD_P<.1]"]
-subjects_selection_crit_dict['F_fd_med_02_1'] = ["df[df.sex == \'F\']", "df[df.n_TRs>294]", "df[df.mean_FD_P<1]",
-                                                 "df[df.mean_FD_P>.2]"]
-subjects_selection_crit_dict['F_fd_med_03_1'] = ["df[df.sex == \'F\']", "df[df.n_TRs>294]", "df[df.mean_FD_P<1]",
-                                                 "df[df.mean_FD_P>.3]"]
-subjects_selection_crit_dict['F_random_sample'] = ["df[df.sex == \'F\']", "df[df.n_TRs>294]", "df[df.random_sample]"]
-subjects_selection_crit_dict['F_uniform_sample'] = ["df[df.sex == \'F\']", "df[df.n_TRs>294]", "df[df.uniform_sample]"]
-
-subjects_selection_crit_dict['F_young'] = ["df[df.sex == \'F\']", "df[df.n_TRs>294]", "df[df.age<55]"]
-
-subjects_selection_crit_dict['M'] = ["df[df.sex == \'M\']", "df[df.n_TRs>294]"]
-subjects_selection_crit_dict['M_fd04'] = ["df[df.sex == \'M\']", "df[df.n_TRs>294]", "df[df.mean_FD_P<.4]"]
-subjects_selection_crit_dict['M_young'] = ["df[df.sex == \'M\']", "df[df.n_TRs>294]", "df[df.age<55]"]
-
-subjects_selection_crit_dict['bothSexes_young'] = ["df[df.n_TRs>294]", "df[df.age<55]"]
-
-subjects_selection_crit_dict['random_sample'] = ["df[df.n_TRs>294]", "df[df.random_sample]"]
-subjects_selection_crit_dict['uniform_sample'] = ["df[df.n_TRs>294]", "df[df.uniform_sample]"]
-
-# subjects_selection_crit_names_list = ['F', 'F_fd04', 'F_fd01', 'F_fd_med_02_1', 'F_fd_med_03_1']#, 'M', 'F_young', 'M_young', 'bothSexes_young']
-# subjects_selection_crit_names_list = [
-#     'bothSexes_FD01_035', 'bothSexes_FD01_035_age50plus']  # ['F', 'random_sample', 'uniform_sample']#, 'M', 'F_young', 'M_young', 'bothSexes_young']
-
-# in_data_name_list = [['lh_ct', 'rh_ct'], ['lh_csa', 'rh_csa'], ['aseg'], ['alff'], ['falff'], ['reho'], ['variability_std'],
-#                      ['craddock_788_noBP'], ['craddock_788_BP'], ['gordon_noBP'], ['gordon_788_BP'],
-#                      # ['falff', 'alff'],
-#                      ['lh_ct', 'rh_ct', 'lh_csa', 'rh_csa', 'aseg'],
-#                      ['lh_ct', 'rh_ct', 'lh_csa', 'rh_csa', 'aseg', 'alff'],
-#                      ['lh_ct', 'rh_ct', 'lh_csa', 'rh_csa', 'aseg', 'falff', 'alff', 'reho', 'variability_std']]  # , ['falff'], ['alff_craddock_788']]
-# in_data_name_list = [['alff_GM_3mm'], ['alff_GM_4mm'], ['alff_GM_8mm'],['alff_GM_WM_3mm'], ['alff_GM_WM_4mm'], ['alff_GM_WM_8mm'], ['alff_brain_mask_3mm'], ['alff_brain_mask_4mm'], ['alff_brain_mask_8mm'], ['alff_brain_mask_3mm_frauke']]
-in_data_name_list = [
-    # ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0'], ['lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0'], ['aseg'],
-    # ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg'],
-    # ['lh_ct_fsav5_sm10', 'rh_ct_fsav5_sm10'], ['lh_csa_fsav5_sm10', 'rh_csa_fsav5_sm10'],
-    # ['lh_ct_fsav5_sm10', 'rh_ct_fsav5_sm10', 'lh_csa_fsav5_sm10', 'rh_csa_fsav5_sm10', 'aseg'],
-    ['alff_GM_WM_4mm_sm0'], ['falff_GM_WM_4mm_sm0'], ['alff_z_GM_WM_4mm_sm0'], ['falff_z_GM_WM_4mm_sm0'],
-    ['alff_GM_WM_4mm_sm8'], ['falff_GM_WM_4mm_sm8'], ['alff_z_GM_WM_4mm_sm8'], ['falff_z_GM_WM_4mm_sm8'],
-    ['reho_GM_WM_4mm_sm0'], ['reho_GM_WM_4mm_sm8'],
-    ['alff_GM_WM_8mm_sm8'], ['falff_GM_WM_8mm_sm8'], ['alff_z_GM_WM_8mm_sm8'], ['falff_z_GM_WM_8mm_sm8'],
-    ['reho_GM_WM_8mm_sm0'], ['reho_GM_WM_8mm_sm8'],
-    ['craddock_205_BP'], ['craddock_788_BP'], ['gordon_BP'], ['gordon_BP_ds'],
-
-    #                    ['aseg'], ['aparc_lh_thickness', 'aparc_rh_thickness'], ['aparc_lh_area', 'aparc_rh_area'],
-    #                    ['aseg', 'aparc_lh_thickness', 'aparc_rh_thickness', 'aparc_lh_area', 'aparc_rh_area'],
-    #                    # ['lh_ct_fsav4_sm0', 'rh_ct_fsav4_sm0'], ['lh_csa_fsav4_sm0', 'rh_csa_fsav4_sm0'], ['aseg'],
-    #                    # ['lh_ct_fsav4_sm0', 'rh_ct_fsav4_sm0', 'lh_csa_fsav4_sm0', 'rh_csa_fsav4_sm0', 'aseg'],
-    #                    # ['lh_ct_fsav4_sm0', 'rh_ct_fsav4_sm0', 'lh_csa_fsav4_sm0', 'rh_csa_fsav4_sm0', 'aseg', 'alff_z_GM_WM_4mm'],
-]
-
-subjects_selection_crit_names_list = ['bothSexes']
+    plt.figure()
+    ax = sns.barplot(x="r2_test", y="ana", data=df_all, hue='FD_res')
+    plt.tight_layout()
+    plt.savefig('00_all_bars_r2.pdf')
 
 
-import warnings
+good_ana = set(df.query('MAE_test < 6').index)
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-learning_prepare_data_wf(working_dir=working_dir,
-                         ds_dir=ds_dir,
-                         template_lookup_dict=template_lookup_dict,
-                         behav_file=behav_file,
-                         qc_file=qc_file,
-                         in_data_name_list=in_data_name_list,
-                         data_lookup_dict=data_lookup_dict,
-                         subjects_selection_crit_dict=subjects_selection_crit_dict,
-                         subjects_selection_crit_names_list=subjects_selection_crit_names_list,
-                         use_n_procs=use_n_procs,
-                         plugin_name=plugin_name)
+df_good = df.loc[good_ana]
+df_good.ix[:,'ana'] = df_good.index.values
+multi_index = pd.MultiIndex.from_tuples(zip(df_good['ana'], df_good['FD_res']), names=['ana_id', 'FD_red'])
+df_good.set_index(multi_index, inplace=True)
+df_good.sort_index(inplace=True)
+
+c = sns.color_palette()
+plt.figure()
+ax = sns.barplot(x="MAE_test", y="ana", data=df_good, hue='FD_res')
+#plt.tight_layout()
+plt.savefig('00_best7_bars_MAE.pdf')
+
+plt.figure()
+ax = sns.barplot(x="r2_test", y="ana", data=df_good, hue='FD_res')
+#plt.tight_layout()
+plt.savefig('00_best7_bars_r2.pdf')
+
+
+f, axarr = plt.subplots(len(good_ana), 2, sharex=True, sharey=True, figsize=(15, 35))
+for row, a in enumerate(good_ana):
+    if len(a)>50:
+        a_str = a[:50] + '\n' + a[50:]
+    else:
+        a_str = a
+
+    ax = axarr[row, 0]
+    sns.regplot(x='age', y='pred_age_test', data=scatter_df[False][a], ax=ax, color=c[0])
+    ax.set_aspect('equal')
+    ax.plot([10, 80], [10, 80], 'k')
+    ax.set_title('%s\nr2 = %3.2f ' % (a_str, df[df.FD_res==False].loc[a]['r2_test']))
+
+    ax = axarr[row, 1]
+    sns.regplot(x='age', y='pred_age_test', data=scatter_df[True][a], ax=ax, color=c[1])
+    ax.set_aspect('equal')
+    ax.plot([10, 80], [10, 80], 'k')
+    ax.set_title('%s\nr2 = %3.2f ' % (a_str, df[df.FD_res==True].loc[a]['r2_test']))
+
+plt.tight_layout()
+plt.savefig('00_best7_scatters.pdf')
+
