58ca0b5903999209575b77b303d0454ad209632a
fliem <franz.liem>
2016-02-20 07:46:14 +0100

    ref: moved functions to helper

--- learning/utils.py
+++ learning/utils.py
@@ -1,4 +1,17 @@
-__author__ = 'franzliem'
+import os
+
+import numpy as np
+import nibabel as nb
+import pandas as pd
+import pickle
+import sklearn
+from nilearn import plotting
+import pylab as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+from learning.helpers import _merge_nii, _merge_matrix, _merge_fs, _merge_fs_tab, _vectorize_nii, _vectorize_matrix, \
+    _vectorize_fs, _vectorize_fs_tab, _vectorize_behav_df
 
 
 def aggregate_data(file_list, df_file, df_col_names):
@@ -10,121 +23,6 @@ def aggregate_data(file_list, df_file, df_col_names):
     special case behav files (detected by df_col_names not None):
     here the data already is aggregated; just pipe through (save)
     '''
-
-    import os
-    import nibabel as nb
-    import numpy as np
-
-    def _merge_nii(file_list, out_filename):
-        '''
-        merges list of nii over subjects
-        '''
-        from nipype.pipeline.engine import Node, Workflow
-        import nipype.interfaces.fsl as fsl
-
-        # For long file lists, nipype throws an error if not split
-        if len(file_list) < 1300:
-            merge = Node(fsl.Merge(dimension='t'), name='merge')
-            merge.base_dir = os.getcwd()
-            merge.inputs.in_files = file_list
-            merge.inputs.merged_file = out_filename
-            result = merge.run()
-        else:
-            merge_1 = Node(fsl.Merge(dimension='t'), name='merge_1')
-            merge_1.base_dir = os.getcwd()
-            merge_1.inputs.in_files = file_list[:1300]
-            merge_1.inputs.merged_file = 'merge_1.nii.gz'
-            result_1 = merge_1.run()
-
-            merge_2 = Node(fsl.Merge(dimension='t'), name='merge_2')
-            merge_2.base_dir = os.getcwd()
-            merge_2.inputs.in_files = file_list[1300:]
-            merge_2.inputs.merged_file = 'merge_2.nii.gz'
-            result_2 = merge_2.run()
-
-            merge = Node(fsl.Merge(dimension='t'), name='merge')
-            merge.base_dir = os.getcwd()
-            merge.inputs.in_files = [result_1.outputs.merged_file, result_2.outputs.merged_file]
-            merge.inputs.merged_file = out_filename
-            result = merge.run()
-
-        return result.outputs.merged_file
-
-    def _merge_matrix(file_list, out_filename):
-        '''
-        merges list of pickled matrices so that -> matrix[subject,x,y]
-        '''
-        import pickle
-        import numpy as np
-        import os
-
-        out_data = None
-
-        for matrix_file in file_list:
-            with open(matrix_file, 'r') as f:
-                in_data = pickle.load(f)
-
-            if out_data is None:
-                out_data = {}
-                for k in in_data.keys():
-                    out_data[k] = in_data[k][np.newaxis, ...]
-            else:
-                for k in in_data.keys():
-                    out_data[k] = np.concatenate((out_data[k], in_data[k][np.newaxis, ...]))
-
-        full_out_filename = os.path.join(os.getcwd(), out_filename)
-        with open(full_out_filename, 'w') as f:
-            pickle.dump(out_data, f)
-
-        return full_out_filename
-
-    def _merge_fs(file_list, out_filename):
-        '''
-        merges list of fs surfs
-        '''
-        import numpy as np
-        import os
-        import nibabel as nb
-
-        out_data = None
-
-        for fs_file in file_list:
-            img = nb.load(fs_file)
-            in_data = img.get_data().squeeze()
-
-            if out_data is None:
-                out_data = in_data[np.newaxis, ...]
-            else:
-                out_data = np.concatenate((out_data, in_data[np.newaxis, ...]))
-
-        full_out_filename = os.path.join(os.getcwd(), out_filename)
-        np.save(full_out_filename, out_data)
-
-        return full_out_filename
-
-    def _merge_fs_tab(file_list, out_filename):
-        '''
-        merges list of fs_tab textfiles
-        '''
-        import numpy as np
-        import os
-        import pandas as pd
-
-        out_data = None
-
-        for i, tab_file in enumerate(file_list):
-            df_single = pd.read_csv(tab_file, index_col=0, delimiter='\t')
-            df_single.index.name = 'subject_id'
-
-            if i == 0:
-                df = df_single
-            else:
-                df = pd.concat([df, df_single])
-        full_out_filename = os.path.join(os.getcwd(), out_filename)
-        df.to_csv(full_out_filename)
-
-        return full_out_filename
-
     out_filename = os.path.basename(file_list[0])
     if file_list[0].endswith('.nii.gz'):  # 3d nii files
         merged_file = _merge_nii(file_list, out_filename)
@@ -152,66 +50,6 @@ def aggregate_data(file_list, df_file, df_col_names):
 
 def vectorize_data(in_data_file, mask_file, matrix_name, parcellation_path, fwhm, use_diagonal,
                    use_fishers_z, df_col_names):
-    import os, pickle
-    import numpy as np
-
-    # LITTLE HELPERS
-    def _vectorize_nii(in_data_file, mask_file, parcellation_path, fwhm):
-        from nilearn.input_data import NiftiMasker, NiftiLabelsMasker
-
-        if parcellation_path is None:
-            masker = NiftiMasker(mask_img=mask_file, smoothing_fwhm=fwhm)
-        else:
-            masker = NiftiLabelsMasker(labels_img=parcellation_path, smoothing_fwhm=fwhm)
-
-        vectorized_data = masker.fit_transform(in_data_file)
-
-        return vectorized_data, masker
-
-    def _vectorize_matrix(in_data_file, matrix_name, use_diagonal=False):
-
-        def _lower_tria_vector(m, use_diagonal=False):
-            '''
-            use_diagonal=False: returns lower triangle of matrix (without diagonale) as vector
-            use_diagonal=True: returns lower triangle of matrix (with diagonale) as vector; e.g. for downsampled matrices
-            matrix dims are x,y,subject
-            '''
-            i = np.ones_like(m).astype(np.bool)
-            if use_diagonal:
-                k = 0
-            else:
-                k = -1
-            tril_ind = np.tril(i, k)
-            vectorized_data = m[tril_ind].reshape(m.shape[0], -1)
-            return vectorized_data
-
-        # load pickled matrix
-        with open(in_data_file, 'r') as f:
-            matrix = pickle.load(f)
-
-        # get lower triangle
-        vectorized_data = _lower_tria_vector(matrix[matrix_name], use_diagonal=use_diagonal)
-
-        return vectorized_data
-
-    def _vectorize_fs(in_data_file):
-        # load fs matrix (is already vectorized)
-        vectorized_data = np.load(in_data_file)
-        return vectorized_data
-
-    def _vectorize_fs_tab(in_data_file):
-        import pandas as pd
-        df = pd.read_csv(in_data_file, index_col=0)
-        vectorized_data = df.values
-        return vectorized_data
-
-    def _vectorize_behav_df(in_data_file, df_col_names):
-        import pandas as pd
-        df = pd.read_pickle(in_data_file)
-        vectorized_data = df[df_col_names].values
-        return vectorized_data
-
-    # RUN
     masker = None
 
     if in_data_file.endswith('.nii.gz'):  # 3d nii files
@@ -235,7 +73,6 @@ def vectorize_data(in_data_file, mask_file, matrix_name, parcellation_path, fwhm
         vectorized_data = _vectorize_behav_df(in_data_file, df_col_names)
         data_type = 'behav'
 
-
     else:
         raise Exception('Cannot guess type from filename: %s' % in_data_file)
 
@@ -254,9 +91,6 @@ def vectorize_data(in_data_file, mask_file, matrix_name, parcellation_path, fwhm
 
 
 def pred_real_scatter(y_test, y_test_predicted, title_str, in_data_name):
-    import os
-    import pylab as plt
-    from matplotlib.backends.backend_pdf import PdfPages
     plt.figure()
     plt.scatter(y_test, y_test_predicted)
     plt.plot([10, 80], [10, 80], 'k')
@@ -274,11 +108,6 @@ def pred_real_scatter(y_test, y_test_predicted, title_str, in_data_name):
 
 
 def plot_brain_age(y_test, y_test_predicted, in_data_name):
-    import os
-    import pylab as plt
-    import numpy as np
-    from matplotlib.backends.backend_pdf import PdfPages
-
     brain_age = y_test_predicted - y_test
     title_str = 'mean brain age: %s\nr = %s' % (brain_age.mean(), np.corrcoef(brain_age, y_test)[0, 1])
     plt.figure()
@@ -302,7 +131,6 @@ def residualize_group_data(signals, confounds):
     returns residualized_signals.shape: subjects x n_data_points
     '''
     from nilearn.signal import clean
-
     residualized_signals = clean(signals, detrend=False, standardize=False, confounds=confounds, low_pass=None,
                                  high_pass=None, t_r=None)
     return residualized_signals
@@ -312,28 +140,9 @@ def backproject_weights_to_full_space(trained_model_file):
     '''
     Takes a vector of weights and fills NaNs for all the features that have been eliminated during preprocessing
     '''
-    import pickle
-    import numpy as np
-    import sklearn
-
     ADD_CONSTANT = 1000
 
     trained_model = pickle.load(open(trained_model_file))
-
-    # step_ind = {}
-    # if isinstance(trained_model, sklearn.grid_search.GridSearchCV): #necessary because gs object is set up diffrently
-    #     steps = trained_model.best_estimator_.steps
-    # else:
-    #     steps = trained_model.steps
-    #
-    # for step_no, step in enumerate(steps):
-    #     step_name, step_content = step
-    #     step_ind[step_name] = step_no
-    #
-    # imp = steps[step_ind['fill_missing']][1]
-    # var_thr = steps[step_ind['var_thr']][1]
-    # scaler = steps[step_ind['normalize']][1]
-    # regression_model = steps[step_ind['regression_model']][1]
 
     if isinstance(trained_model, sklearn.grid_search.GridSearchCV):  # necessary because gs object is set up diffrently
         steps = trained_model.best_estimator_.named_steps
@@ -361,8 +170,6 @@ def backproject_weights_to_full_space(trained_model_file):
     else:
         raise Exception('neither rfe nor regression_model found in steps.keys() %s' % steps.keys())
 
-
-
     # add constant to the weights in order to differentiate between 0 weights and zeros that have been iputed in the
     # case of 0-variance features
     # after replacing 0s with nans, substract constant
@@ -376,7 +183,6 @@ def backproject_weights_to_full_space(trained_model_file):
 
     # set sparse weights to nan
     mapped_back[sparse_ind] = np.nan
-
     return mapped_back
 
 
@@ -386,7 +192,6 @@ def vector_to_matrix(v, use_diagonal=False):
     vector_size = matrix_size*(matrix_size-1)*.5
     matrix diagonal is set to 0
     '''
-    import numpy as np
     vector_size = v.shape[0]
     if use_diagonal:
         diag_add = -1
@@ -411,7 +216,6 @@ def matrix_to_vector(m):
     '''
     returns lower triangle of 2D matrix (without diagonale) as vector
     '''
-    import numpy as np
     i = np.ones_like(m).astype(np.bool)
     tril_ind = np.tril(i, -1)
     v = m[tril_ind]
@@ -422,7 +226,6 @@ def test_vector_to_matrix():
     '''
     tests vector_to_matrix() and matrix_to_vector()
     '''
-    import numpy as np
     # simulate data
     matrix_size = 200
     m_in = np.random.randn(matrix_size, matrix_size)
@@ -438,13 +241,6 @@ def save_weights(data, data_type, save_template, outfile_name, masker=None):
     '''
     saves weights as nii or other file
     '''
-    import nibabel as nb
-    import numpy as np
-    import pandas as pd
-    import os
-    from nilearn import plotting
-    import pylab as plt
-
     weights_file_str = '_weights'
     render_file_str = '_rendering'
     outfile_render = os.path.abspath(outfile_name + render_file_str + '.pdf')
