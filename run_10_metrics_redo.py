1c69f1a984522330b5060cc299c052881c4775a5
fliem <franz.liem>
2016-02-26 18:48:30 +0100

    msdl metrcis added

--- run_10_metrics_redo.py
+++ run_10_metrics_redo.py
@@ -3,11 +3,13 @@ wd ca 170MB/subj
 '''
 import os
 from metrics.calc_metrics_redo import calc_local_metrics
-from variables import template_dir, in_data_root_path, subjects_list, metrics_root_path, wd_root_path, selectfiles_templates
+from variables import template_dir, subjects_list, metrics_root_path, wd_root_path, selectfiles_templates
 from utils import load_subjects_list
 
-subject_file = '/scr/adenauer2/Franz/LIFE16/redo_metrics.txt'
+
+subject_file = '/scr/adenauer2/Franz/LIFE16/LIFE16_preprocessed_subjects_list_n2557.txt'
 subjects_list = load_subjects_list(subject_file)
+
 #
 # # CHANCE FOLD HERE
 # fold = 0
@@ -29,26 +31,15 @@ working_dir_base = os.path.join(wd_root_path, 'wd_metrics')
 ds_dir_base = os.path.join(metrics_root_path, 'metrics')
 
 brain_mask = '/scr/adenauer2/Franz/LIFE16/Templates/MNI_resampled_brain_mask.nii'
+gm_wm_mask = '/home/raid2/liem/PowerFolders/Workspace/LeiCA_LIFE/templates/Templates/MNI152_T1_3mm_GM_WM.nii.gz'
 
 template_dir = os.path.join(template_dir, 'parcellations')
 
-# con mat parameters
-bp_freq_list = [(None, None), (0.01, 0.1)]
-TR = 2.0
-
-parcellations_dict = {}
-parcellations_dict['craddock_205'] = {
-    'nii_path': os.path.join(template_dir,
-                             'craddock_2012/scorr_mean_single_resolution/scorr_mean_parc_n_21_k_205_rois.nii.gz'),
-    'is_probabilistic': False}
-parcellations_dict['craddock_788'] = {
-    'nii_path': os.path.join(template_dir,
-                             'craddock_2012/scorr_mean_single_resolution/scorr_mean_parc_n_43_k_788_rois.nii.gz'),
-    'is_probabilistic': False}
-parcellations_dict['gordon'] = {
-    'nii_path': os.path.join(template_dir, 'Gordon_2014_Parcels/Parcels_MNI_111_sorted.nii.gz'),
-    'is_probabilistic': False}
-
+selectfiles_templates = {
+    'alff': '{subject_id}/alff/alff.nii.gz',
+    'falff': '{subject_id}/alff/falff.nii.gz',
+    'variability': '{subject_id}/variability/ts_std.nii.gz',
+}
 use_n_procs = 5
 #plugin_name = 'MultiProc'
 plugin_name = 'CondorDAGMan'
@@ -59,12 +50,9 @@ for subject_id in subjects_list:
     ds_dir = os.path.join(ds_dir_base, subject_id)
 
     print('\n\nsubmitting %s'%subject_id)
-    calc_local_metrics(brain_mask=brain_mask,
-                       preprocessed_data_dir=in_data_root_path,
+    calc_local_metrics(gm_wm_mask=gm_wm_mask,
+                       preprocessed_data_dir=ds_dir_base,
                        subject_id=subject_id,
-                       parcellations_dict=parcellations_dict,
-                       bp_freq_list=bp_freq_list,
-                       TR=TR,
                        selectfiles_templates=selectfiles_templates,
                        working_dir=working_dir,
                        ds_dir=ds_dir,
--- run_10_metrics_redo_msdl.py
+++ run_10_metrics_redo_msdl.py
@@ -6,33 +6,19 @@ from metrics.calc_metrics import calc_local_metrics
 from variables import template_dir, in_data_root_path, subjects_list, \
     metrics_root_path, wd_root_path, selectfiles_templates
 
-working_dir_base = os.path.join(wd_root_path, 'wd_metrics')
+working_dir_base = os.path.join(wd_root_path, 'wd_metrics_msdl')
+#fixme
 ds_dir_base = os.path.join(metrics_root_path, 'metrics_test')
-
+subjects_list = subjects_list[:3]
 brain_mask = '/scr/adenauer2/Franz/LIFE16/Templates/MNI_resampled_brain_mask.nii'
 
 template_dir = os.path.join(template_dir, 'parcellations')
 
 # con mat parameters
-bp_freq_list = [(None, None), (0.01, 0.1)]
+bp_freq_list = [(0.01, 0.1)]
 TR = 2.0
 
 parcellations_dict = {}
-parcellations_dict['craddock_205'] = {
-    'nii_path': os.path.join(template_dir,
-                             'craddock_2012/scorr_mean_single_resolution/scorr_mean_parc_n_21_k_205_rois.nii.gz'),
-    'is_probabilistic': False}
-
-parcellations_dict['craddock_788'] = {
-    'nii_path': os.path.join(template_dir,
-                             'craddock_2012/scorr_mean_single_resolution/scorr_mean_parc_n_43_k_788_rois.nii.gz'),
-    'is_probabilistic': False}
-
-parcellations_dict['gordon'] = {
-    'nii_path': os.path.join(template_dir,
-                             'Gordon_2014_Parcels/Parcels_MNI_111_sorted.nii.gz'),
-    'is_probabilistic': False}
-
 parcellations_dict['msdl_abide'] = {
     'nii_path': os.path.join(template_dir,
                              'msdl_abide_2016/MSDL_ABIDE/maps.nii'),
