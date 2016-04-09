85a18516c10efef1b6c1d8f81fcbb3da72489654
fliem <franz.liem>
2016-04-09 06:22:59 +0200

    rfe to univar

--- learning/learning_utils.py
+++ learning/learning_utils.py
@@ -90,7 +90,7 @@ def run_prediction_split_fct(X_file, target_name, selection_criterium, df_file, 
     from LeiCA_LIFE.learning.learning_utils import pred_real_scatter, plot_brain_age, residualize_group_data
     from sklearn.decomposition import PCA
     from sklearn.feature_selection import RFE
-
+    from sklearn.feature_selection import SelectKBest, f_regression
     data_str = target_name + '__' + selection_criterium + '__' + data_str
 
     df = pd.read_pickle(df_file)
@@ -136,7 +136,7 @@ def run_prediction_split_fct(X_file, target_name, selection_criterium, df_file, 
     else:
         raise Exception('scaler not detected: %s' % scaler)
 
-    regression_model = SVR(kernel='linear')
+    regression_model = SVR(kernel='linear', cache_size=1000)
     # fixme
     # C = 50000
     # epsilon = .1
@@ -152,12 +152,11 @@ def run_prediction_split_fct(X_file, target_name, selection_criterium, df_file, 
         n_features = X.shape[1]
         n_features_to_select = int(n_features * .2)
         if n_features_to_select > 0:  # only perform rfe if features remain
-            eliminate = RFE(estimator=regression_model, n_features_to_select=n_features_to_select, step=.5)
-            pipeline_list.append(('rfe', eliminate))
-        else:
-            pipeline_list.append(('regression_model', regression_model))
-    else:
-        pipeline_list.append(('regression_model', regression_model))
+            anova_filter = SelectKBest(f_regression, k=n_features_to_select)
+            #eliminate = RFE(estimator=regression_model, n_features_to_select=n_features_to_select, step=.5)
+            pipeline_list.append(('anova_filter', anova_filter))
+
+    pipeline_list.append(('regression_model', regression_model))
 
     pipeline = Pipeline(pipeline_list)
 
@@ -169,38 +168,99 @@ def run_prediction_split_fct(X_file, target_name, selection_criterium, df_file, 
         }
         pipe = GridSearchCV(pipeline, params, cv=5,
                             scoring='mean_absolute_error', n_jobs=2)
-
+    elif strat_split:
+        pass # create pipe later
     else:
         pipe = pipeline
 
     if strat_split:
+        # n_bins = 10
+        # df['mean_FD_P_bins'] = pd.cut(df['mean_FD_P'], n_bins, labels=range(n_bins))
+        # cv = StratifiedShuffleSplit(df['mean_FD_P_bins'].values, 10, test_size=0.5, random_state=0)
+        # pipe.fit(X, y, cv=cv)
+        # r2 = cross_val_score(pipe, X, y, cv=cv, scoring='r2')
+        # mae = cross_val_score(pipe, X, y, cv=cv, scoring='mean_absolute_error')
+        from sklearn.ensemble import BaggingRegressor
         n_bins = 10
         df['mean_FD_P_bins'] = pd.cut(df['mean_FD_P'], n_bins, labels=range(n_bins))
-        cv = StratifiedShuffleSplit(df['mean_FD_P_bins'].values, 10, test_size=0.5, random_state=0)
-        # pipe.fit(X, y, cv=cv)
-        r2 = cross_val_score(pipe, X, y, cv=cv, scoring='r2')
-        mae = cross_val_score(pipe, X, y, cv=cv, scoring='mean_absolute_error')
+        cv = StratifiedShuffleSplit(df['mean_FD_P_bins'].values, 2, test_size=0.5, random_state=0)
+        ind_train = cv.y==0
+        X_train = X[ind_train,:]
+        y_train = y[ind_train]
+        confounds_train = confounds[ind_train]
 
-        train_r2 = np.nan
+        ind_test = cv.y==1
+        X_test = X[ind_test,:]
+        y_test = y[ind_test]
+        confounds_test = confounds[ind_test]
+
+        df.ix[ind_train, ['split_group']] = 'train'
+        df.ix[ind_test, ['split_group']] = 'test'
+
+        # REGRESS OUT CONFOUNDS IF NEEDED
+        if regress_confounds:
+            X_train = residualize_group_data(X_train, confounds_train)
+            X_test = residualize_group_data(X_test, confounds_test)
+
+        pipe = BaggingRegressor(base_estimator=pipeline, n_estimators=10, max_samples=.75)
+        pipe.fit(X_train, y_train)
+        y_predicted = pipe.predict(X_test)
+
+        df.ix[ind_train, ['pred_age_train']] = np.nan
+        df.ix[ind_test, ['pred_age_test']] = y_predicted
+
+        test_mae = mean_absolute_error(y_test, y_predicted)
+        test_r2 = r2_score(y_test, y_predicted)
+        test_rpear2 = np.corrcoef(y_test, y_predicted)[0, 1]
+
         train_mae = np.nan
-        test_r2 = r2.mean()
-        test_r2_std = r2.std()
-        test_mae = np.abs(mae.mean())
-        test_mae_std = mae.std()
-        test_rpear2 = np.nan
+        train_r2 = np.nan
 
-        out_str = 'r2:  M = %0.7f \t SD = %0.7f\n' % (test_r2, test_r2_std)
-        out_str += 'mae: M = %0.7f \t SD = %0.7f\n' % (test_mae, test_mae_std)
-        scatter_file = os.path.abspath('score_%s.txt' % data_str)
-        with open(scatter_file, 'w') as fi:
-            fi.write(out_str)
+        test_r2_std = np.nan
+        test_mae_std = np.nan
 
+        # train_r2 = np.nan
+        # train_mae = np.nan
+        # test_r2 = r2.mean()
+        # test_r2_std = r2.std()
+        # test_mae = np.abs(mae.mean())
+        # test_mae_std = mae.std()
+        # test_rpear2 = np.nan
+        #
+        # out_str = 'r2:  M = %0.7f \t SD = %0.7f\n' % (test_r2, test_r2_std)
+        # out_str += 'mae: M = %0.7f \t SD = %0.7f\n' % (test_mae, test_mae_std)
+        # scatter_file = os.path.abspath('score_%s.txt' % data_str)
+        # with open(scatter_file, 'w') as fi:
+        #     fi.write(out_str)
+        #
+        #
+        # # fixme needed:?
+        # empty_file = os.path.abspath('empty.txt')
+        # with open(empty_file, 'w') as fi:
+        #     fi.write('')
+        # brain_age_scatter_file = df_use_file = empty_file  # or = ''
+        ###
 
-        # fixme needed:?
-        empty_file = os.path.abspath('empty.txt')
-        with open(empty_file, 'w') as fi:
-            fi.write('')
-        brain_age_scatter_file = df_use_file = empty_file  # or = ''
+        # test_mae_list = []
+        # test_r2_list = []
+        # test_rpear2_list = []
+        # coef_list = []
+        # intercept_list=[]
+        # for train_index, test_index in cv:
+        #     X_train, X_test = X[train_index], X[test_index]
+        #     y_train, y_test = y[train_index], y[test_index]
+        #
+        #     pipe.fit(X_train, y_train)
+        #     y_predicted_train = pipe.predict(X_train)
+        #     y_predicted = pipe.predict(X_test)
+        #
+        #     test_mae_list.append(mean_absolute_error(y_test, y_predicted))
+        #     test_r2_list.append(r2_score(y_test, y_predicted))
+        #     test_rpear2_list.append(np.corrcoef(y_test, y_predicted)[0, 1])
+        #
+        #     coef_list.append(backproject_weights(pipe))
+        #     intercept_list.append(pipe.named_steps['regression_model'].intercept_)
+
 
     else:
         pipe.fit(X_train, y_train)
@@ -220,25 +280,25 @@ def run_prediction_split_fct(X_file, target_name, selection_criterium, df_file, 
         test_r2_std = np.nan
         test_mae_std = np.nan
 
-        title_str = 'r2: {:.3f} MAE:{:.3f}'.format(test_r2, test_mae)
-        scatter_file = pred_real_scatter(y_test, y_predicted, title_str, data_str)
+    title_str = 'r2: {:.3f} MAE:{:.3f}'.format(test_r2, test_mae)
+    scatter_file = pred_real_scatter(y_test, y_predicted, title_str, data_str)
 
-        # grid scores as textfile
-        if use_grid_search:
-            sorted_grid_score = sorted(pipe.grid_scores_,
-                                       key=lambda x: x.mean_validation_score,
-                                       reverse=True)
-            score_str = [str(n) + ': ' + str(g) for n, g in
-                         enumerate(sorted_grid_score)]
-            gs_text_file = os.path.abspath('gs_txt_' + data_str + '.txt')
-            with open(gs_text_file, 'w') as f:
-                f.write('\n'.join(score_str))
-            scatter_file = [scatter_file, gs_text_file]
+    # grid scores as textfile
+    if use_grid_search:
+        sorted_grid_score = sorted(pipe.grid_scores_,
+                                   key=lambda x: x.mean_validation_score,
+                                   reverse=True)
+        score_str = [str(n) + ': ' + str(g) for n, g in
+                     enumerate(sorted_grid_score)]
+        gs_text_file = os.path.abspath('gs_txt_' + data_str + '.txt')
+        with open(gs_text_file, 'w') as f:
+            f.write('\n'.join(score_str))
+        scatter_file = [scatter_file, gs_text_file]
 
-        brain_age_scatter_file = plot_brain_age(y_test, y_predicted, data_str)
+    brain_age_scatter_file = plot_brain_age(y_test, y_predicted, data_str)
 
-        df_use_file = os.path.join(os.getcwd(), data_str + '_df_predicted.pkl')
-        df.to_pickle(df_use_file)
+    df_use_file = os.path.join(os.getcwd(), data_str + '_df_predicted.pkl')
+    df.to_pickle(df_use_file)
 
     # performace results df
     df_res_out_file = os.path.abspath(data_str + '_df_results.pkl')
@@ -336,16 +396,14 @@ def backproject_and_split_weights_fct(trained_model_file, multimodal_backproject
     return out_file_list, out_file_render_list
 
 
-def backproject_weights_to_full_space(trained_model_file):
+def backproject_weights(trained_model):
     '''
     Takes a vector of weights and fills NaNs for all the features that have been eliminated during preprocessing
     '''
-    import pickle
     import numpy as np
     import sklearn
 
     ADD_CONSTANT = 1000
-    trained_model = pickle.load(open(trained_model_file))
 
     if isinstance(trained_model, sklearn.grid_search.GridSearchCV):  # necessary because gs object is set up diffrently
         steps = trained_model.best_estimator_.named_steps
@@ -355,18 +413,17 @@ def backproject_weights_to_full_space(trained_model_file):
     imp = steps['fill_missing']
     var_thr = steps['var_thr']
     scaler = steps['normalize']
+    regression_model = steps['regression_model']
 
-    if 'rfe' in steps.keys():
-        rfe = steps['rfe']
-        regression_model = rfe.estimator_
-        # back trainsform weights from sparse (rfe) to full
+    if 'anova_filter' in steps.keys():
+        # back trainsform weights from sparse (anova selection) to full
         # insert nans in eliminated features
-        weights = rfe.inverse_transform(regression_model.coef_ + ADD_CONSTANT)
+        anova_filter = steps['anova_filter']
+        weights = anova_filter.inverse_transform(regression_model.coef_ + ADD_CONSTANT)
         sparse_ind = (weights == 0)
         weights -= ADD_CONSTANT
 
     elif 'regression_model' in steps.keys():
-        regression_model = steps['regression_model']
         weights = regression_model.coef_
         sparse_ind = np.zeros_like(weights).astype(np.bool)
 
@@ -387,6 +444,16 @@ def backproject_weights_to_full_space(trained_model_file):
     # set sparse weights to nan
     mapped_back[sparse_ind] = np.nan
     return mapped_back
+
+
+def backproject_weights_to_full_space(trained_model_file):
+    '''
+    Loads trained model and call backproject_weights
+    '''
+    import pickle
+    from LeiCA_LIFE.learning.learning_utils import backproject_weights
+    trained_model = pickle.load(open(trained_model_file))
+    return backproject_weights(trained_model)
 
 
 def save_weights(data, data_type, save_template, outfile_name, masker=None):
@@ -479,6 +546,7 @@ def run_prediction_from_trained_model_fct(trained_model_file, X_file, target_nam
     import numpy as np
     import pandas as pd
     from sklearn.metrics import r2_score, mean_absolute_error
+    import sklearn.ensemble.bagging
     from LeiCA_LIFE.learning.learning_utils import pred_real_scatter, plot_brain_age, residualize_group_data
 
     data_str = target_name + '__' + selection_criterium + '__' + data_str
@@ -496,6 +564,15 @@ def run_prediction_from_trained_model_fct(trained_model_file, X_file, target_nam
 
     with open(trained_model_file, 'r') as f:
         pipe = pickle.load(f)
+
+    # bagging can't deal with missing values-> impute
+    if np.any(np.isnan(X)) & isinstance(pipe, sklearn.ensemble.bagging.BaggingRegressor):
+        # get mean of means
+        statistics_ = np.asarray([estimator.named_steps['fill_missing'].statistics_ for estimator in pipe.estimators_]).mean(0)
+        fill_matrix = np.repeat([statistics_],X.shape[0], axis=0)
+        X[np.isnan(X)] = fill_matrix[np.isnan(X)]
+
+
 
     # RUND PREDICTION
     y_predicted = pipe.predict(X)
@@ -523,7 +600,7 @@ def run_prediction_from_trained_model_fct(trained_model_file, X_file, target_nam
     df_res_out_file = os.path.abspath(data_str + '_df_results.pkl')
     df_res = pd.DataFrame(
         {'FD_res': regress_confounds, 'r2_train': [train_r2], 'MAE_train': [train_mae],
-         'r2_test': [test_r2], 'r2_test_std': [test_r2_std],'rpear2_test': [test_rpear2],
+         'r2_test': [test_r2], 'r2_test_std': [test_r2_std], 'rpear2_test': [test_rpear2],
          'MAE_test': [test_mae], 'MAE_test_std': [test_mae_std]},
         index=[data_str])
     df_res.to_pickle(df_res_out_file)
