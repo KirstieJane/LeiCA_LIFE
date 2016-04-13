f0e39152d9a7aa75b25e0e9f89755e761f42fc2b
fliem <franz.liem>
2016-04-13 05:04:06 +0200

    updated list

--- learning/learning_variables.py
+++ learning/learning_variables.py
@@ -54,66 +54,61 @@ subjects_selection_crit_dict['bothSexes_neuH'] = ["n_TRs > 294", "neurol_healthy
 #     ['behav_wml_wm_total'], ['behav_wml_wmh_norm'], ['behav_wml_wmh_norm_ln'], ['behav_wml_wmh_ln'],
 #     ['behav_wml_fazekas']
 # ]
+#
+# in_data_name_list = [
+#     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg'],
+#
+#     ['alff_z_GM_WM_3mm_sm0'],
+#     ['falff_z_GM_WM_3mm_sm0'],
+#     ['reho_GM_WM_3mm_sm0'],
+#     ['variability_std_z_GM_WM_3mm_sm0'],
+#
+#     ['alff_z_GM_3mm_sm0'],
+#     ['falff_z_GM_3mm_sm0'],
+#     ['reho_GM_3mm_sm0'],
+#     ['variability_std_z_GM_3mm_sm0'],
+#     ['craddock_205_BP'],
+#     ['craddock_788_BP'],
+#     #
+#     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'alff_z_GM_3mm_sm0'],
+#     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'falff_z_GM_3mm_sm0'],
+#     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'reho_GM_3mm_sm0'],
+#     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg',
+#      'variability_std_z_GM_3mm_sm0'],
+#     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'craddock_205_BP'],
+#     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'craddock_788_BP'],
+#     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'behav_wml_wmh_norm_ln'],
+#     #
+#     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg',
+#      'alff_z_GM_3mm_sm0',
+#      'falff_z_GM_3mm_sm0',
+#      'reho_GM_3mm_sm0',
+#      'variability_std_z_GM_3mm_sm0',
+#      'craddock_788_BP'],
+#     #
+#     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg',
+#      'alff_z_GM_3mm_sm0',
+#      'craddock_788_BP'],
+#     # # ['craddock_205_BP_scr05'], ['craddock_788_BP_scr05'],
+#     # #
+#     ['behav_wml_wm_total'], ['behav_wml_wmh_norm'], ['behav_wml_wmh_norm_ln'], ['behav_wml_wmh_ln'],
+#     ['behav_wml_fazekas'],
+#     ['behav_wml_wmh_norm_ln', 'behav_wml_fazekas'],
+# ]
+#
+# in_data_name_list = [['gordon_BP'], ['gordon_BP_ds'],
+#                      ['craddock_205_BP_scr05'], ['craddock_788_BP_scr05'], ['gordon_BP_scr05'],
+#                      ['msdl_abide_BP']]
 
 in_data_name_list = [
     ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg'],
-
-    ['alff_z_GM_WM_3mm_sm0'],
-    ['falff_z_GM_WM_3mm_sm0'],
-    ['reho_GM_WM_3mm_sm0'],
-    ['variability_std_z_GM_WM_3mm_sm0'],
-
-    ['alff_z_GM_3mm_sm0'],
-    ['falff_z_GM_3mm_sm0'],
-    ['reho_GM_3mm_sm0'],
-    ['variability_std_z_GM_3mm_sm0'],
     ['craddock_205_BP'],
     ['craddock_788_BP'],
-    #
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'alff_z_GM_3mm_sm0'],
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'falff_z_GM_3mm_sm0'],
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'reho_GM_3mm_sm0'],
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg',
-     'variability_std_z_GM_3mm_sm0'],
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'craddock_205_BP'],
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'craddock_788_BP'],
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'behav_wml_wmh_norm_ln'],
-    #
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg',
-     'alff_z_GM_3mm_sm0',
-     'falff_z_GM_3mm_sm0',
-     'reho_GM_3mm_sm0',
-     'variability_std_z_GM_3mm_sm0',
-     'craddock_788_BP'],
-    #
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg',
-     'alff_z_GM_3mm_sm0',
-     'craddock_788_BP'],
-    # # ['craddock_205_BP_scr05'], ['craddock_788_BP_scr05'],
-    # #
-    ['behav_wml_wm_total'], ['behav_wml_wmh_norm'], ['behav_wml_wmh_norm_ln'], ['behav_wml_wmh_ln'],
-    ['behav_wml_fazekas'],
-    ['behav_wml_wmh_norm_ln', 'behav_wml_fazekas'],
-]
-
-in_data_name_list = [['gordon_BP'], ['gordon_BP_ds'],
-                     ['craddock_205_BP_scr05'], ['craddock_788_BP_scr05'], ['gordon_BP_scr05'],
-                     ['msdl_abide_BP']]
-
-in_data_name_list = [
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg'],
-    ['alff_z_GM_WM_3mm_sm0'],
-    ['alff_z_GM_3mm_sm0'],
-    ['craddock_205_BP'],
-    ['craddock_788_BP'],
+    ['craddock_205_BP_scr05'], ['craddock_788_BP_scr05'],
     ['msdl_abide_BP'],
-    ['gordon_BP'], ['gordon_BP_ds'],
-
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'alff_z_GM_3mm_sm0'],
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'craddock_205_BP'],
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'craddock_788_BP'],
-    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0', 'lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0', 'aseg', 'alff_z_GM_3mm_sm0',
-     'craddock_205_BP'],
+    ['lh_ct_fsav5_sm0', 'rh_ct_fsav5_sm0'],
+    ['lh_csa_fsav5_sm0', 'rh_csa_fsav5_sm0'],
+    ['aseg'],
 ]
 
 
